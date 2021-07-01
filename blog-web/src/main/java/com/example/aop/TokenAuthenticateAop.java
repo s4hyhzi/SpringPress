e773fa2ab666a9763aa8f06a286f092df94b79f0
s4hyhzi <ow666faka@qq.com>
2021-07-01 11:14:44 +0800

    jwt用户信息校验

--- blog-web/src/main/java/com/example/aop/TokenAuthenticateAop.java
+++ blog-web/src/main/java/com/example/aop/TokenAuthenticateAop.java
@@ -1,6 +1,9 @@
 package com.example.aop;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.annotation.TokenCheckAnnotation;
+import com.example.blogdao.entity.User;
+import com.example.blogservice.UserService;
 import com.example.entity.ServerEnum;
 import com.example.exception.AuthenticateException;
 import com.example.utils.JwtUtils;
@@ -8,6 +11,7 @@ import com.example.utils.ResponseServer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -17,6 +21,9 @@ import javax.servlet.http.HttpServletRequest;
 @Aspect // 作用是把当前类标识为一个切面供容器读取
 @Component // 把当前类交给spring管理
 public class TokenAuthenticateAop {
+    @Autowired
+    UserService userService;
+
     @Around(value = "execution(* com.example.controller.*.*(..)) && @annotation(tokenCheckAnnotation)")
     public Object tokenAuth(ProceedingJoinPoint joinPoint, TokenCheckAnnotation tokenCheckAnnotation) {
         Object proceed = null;
@@ -32,6 +39,13 @@ public class TokenAuthenticateAop {
         ResponseServer responseServer = JwtUtils.verify(token);
         if (responseServer.getCode() != 200) {
             throw new AuthenticateException(ServerEnum.LOGIN_EXPIRED);
+        }
+        DecodedJWT decodedJWT = (DecodedJWT) responseServer.getData();
+        String userName=decodedJWT.getClaim("userName").asString();
+        System.out.println(userName);
+        User user=userService.findByName(userName);
+        if (user==null){
+            throw new RuntimeException("用户不存在，请重新登录");
         }
 
         // 执行目标方法
